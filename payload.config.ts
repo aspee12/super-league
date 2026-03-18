fe193f538acfd534e7f8faa87d74621083c9aae4
aspee12 <sphuntsho900@gmail.com>
2026-03-18 12:11:48 +0600

    fix(apps): added for dev log

--- payload.config.ts
+++ payload.config.ts
@@ -1,6 +1,7 @@
 import sharp from 'sharp'
 import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import { buildConfig } from 'payload'
+import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
 import { Users } from './src/collections/Users'
 import { Teams } from './src/collections/Teams'
 import { Matches } from './src/collections/Matches'
@@ -9,17 +10,17 @@ import { Media } from './src/collections/Media'
 
 export default buildConfig({
   collections: [Users, Teams, Matches, Players, Media],
-
-  // Your Payload secret - should be a complex and secure string, unguessable
   secret: process.env.PAYLOAD_SECRET || '',
-  // Whichever Database Adapter you're using should go here
-  // Mongoose is shown as an example, but you can also use Postgres
   db: mongooseAdapter({
     url: process.env.DATABASE_URL || '',
   }),
-  // If you want to resize images, crop, set focal point, etc.
-  // make sure to install it and pass it to the config.
-  // This is optional - if you don't need to do these things,
-  // you don't need it!
   sharp,
+  plugins: [
+    vercelBlobStorage({
+      collections: {
+        media: true,
+      },
+      token: process.env.BLOB_READ_WRITE_TOKEN || '',
+    }),
+  ],
 })
