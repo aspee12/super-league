b5660b3f51f68ab44d3f8fc693f9502a1563e3b9
aspee12 <sphuntsho900@gmail.com>
2026-03-19 10:42:45 +0600

    fix(apps): action fail

--- payload.config.ts
+++ payload.config.ts
@@ -26,7 +26,6 @@ export default buildConfig({
         },
       },
       token: process.env.BLOB_READ_WRITE_TOKEN!,
-      access: 'private',
     }),
   ],
   // If you want to resize images, crop, set focal point, etc.
